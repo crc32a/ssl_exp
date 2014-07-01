8aa36b2b241060f9521832df776acd584ab69fdc
Carlos D. Garza <carlos.garza@rackspace.com>
2014-07-01 18:52:29 -0500

    pfft

--- test.py
+++ test.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 
 import utils.ssl
+import sys
 
 xu = utils.ssl.X509()
 
-xu.load_file('./long_date.pem')
+if len(sys.argv) >= 2:
+    file_name = sys.argv[1]
+else:
+    file_name = './cr1.pem'
 
-nb = xu.not_before()
-na = xu.not_after()
+xu.load_file(file_name)
+(nb, na) = xu.not_before_after()
+
+x = xu.x509
+
+sys.stdout.write("not befor: {0}\n".format(nb))
+sys.stdout.write("not after: {0}\n".format(na))
+
+alt_names = xu.get_subject_alt_name()
+for (alt_name_type, alt_name_val) in alt_names:
+    sys.stdout.write("{0}: {1}\n".format(alt_name_type,alt_name_val))
+
--- utils/ssl.py
+++ utils/ssl.py
@@ -6,42 +6,69 @@ import sys
 import os
 import re
 
-generalized_fmt  = "([0-9]{4})([0-9]{2})([0-9]{2})"
-generalized_fmt += "([0-9]{2})([0-9]{2})([0-9]{2})Z"
+generalized_fmt = "([0-9]{4})([0-9]{2})([0-9]{2})" \
+                  "([0-9]{2})([0-9]{2})([0-9]{2})(.)"\
+                  "(([0-9]{2})([0-9]{2}))?"
 
 generalized_time_re = re.compile(generalized_fmt, re.IGNORECASE)
 
+
 def full_path(file_name):
     return os.path.abspath(os.path.expanduser(file_name))
+
 
 class X509(object):
     def __init__(self):
         self.x509 = None
 
+    def not_before_after(self):
+        not_before = self.gmtime_to_datetime(self.x509.get_notBefore())
+        not_after = self.gmtime_to_datetime(self.x509.get_notAfter())
+        return not_before, not_after
 
-    def not_before(self):
-        return self.gmtime_to_datetime(self.x509.get_notBefore())
+    def get_subject_alt_name(self):
+        alt_names = []
+        ext_count = self.x509.get_extension_count()
+        for i in xrange(0, ext_count):
+            ext = self.x509.get_extension(i)
+            if ext.get_short_name() == 'subjectAltName':
+                for alt_name in str(ext).split(", "):
+                    alt_name_type, alt_name_val = alt_name.split(":")
+                    alt_names.append((alt_name_type,alt_name_val))
+        return alt_names
 
-    def not_after(self):
-        return self.gmtime_to_datetime(self.x509.get_notAfter())
-
-    def gmtime_to_datetime(self, general_time):
+    @staticmethod
+    def gmtime_to_datetime(general_time):
         m = generalized_time_re.match(general_time)
         if not m:
-            raise IOError("could not parse general time")
+            raise IOError('could not parse general time')
         day_val = (int(m.group(1)), int(m.group(2)), int(m.group(3)))
         time_val = (int(m.group(4)), int(m.group(5)), int(m.group(6)))
         dt_args = day_val + time_val
-        return datetime.datetime(*dt_args,tzinfo=tzutc())
+        dt = datetime.datetime(*dt_args,tzinfo=tzutc())
+        if m.group(7).lower() == 'z':
+            return dt
+        if m.group(7) == '+':
+            diff_hour = 0 - datetime.timedelta(hours=int(m.group(9)))
+            diff_min = 0 - datetime.timedelta(minutes=int(m.group(10)))
+            return dt + diff_hour + diff_min
+        if m.group(7) == '-':
+            diff_hour = datetime.timedelta(hours=int(m.group(9)))
+            diff_min = datetime.timedelta(minutes=int(m.group(10)))
+            return dt + diff_hour + diff_min
+        raise IOError('Unabled to decode generalized time')
 
-    def load_string(self,buff,filetype=OpenSSL.crypto.FILETYPE_PEM):
+    def loads(self, buff, filetype=OpenSSL.crypto.FILETYPE_PEM):
         self.x509 = OpenSSL.crypto.load_certificate(filetype, buff)
         return self.x509
 
-    def load_file(self,file_name):
+    def dumps(self,filetype=OpenSSL.crypto.FILETYPE_PEM):
+        return OpenSSL.crypto.dump_certificate(filetype,self.x509)
+
+    def load_file(self, file_name, filetype=OpenSSL.crypto.FILETYPE_PEM):
         with open(file_name,'rb') as fp:
             buff = fp.read()
         if six.PY3:
             buff = buff.decode()
-        return self.load_string(buff)
+        return self.loads(buff, filetype=filetype)
 
